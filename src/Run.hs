1b372887565e397649c67c756d0ab4339d49849e
Rickard Andersson <gonz@severnatazvezda.com>
2021-03-09 14:05:14 +0200

    add authentication commands & state

--- src/Run.hs
+++ src/Run.hs
@@ -4,15 +4,18 @@
 
 module Run (run) where
 
+import Data.UUID (UUID)
+import qualified Data.UUID as UUID
+import qualified Data.UUID.V4 as UUID
 import Discord (RunDiscordOpts (..))
 import qualified Discord
 import qualified Discord.Types as Discord
+import qualified Discord.Requests as DiscordRequests
 import Import
 import qualified RIO.Map as Map
 import qualified RIO.Set as Set
 import qualified RIO.Text as Text
 import qualified System.Environment as Environment
-import Data.UUID (UUID)
 
 data BotState = BotState
   { messageCount :: Int,
@@ -43,8 +46,14 @@ eventHandler logFunction botState (Discord.MessageCreate message)
 eventHandler _ _ _ = pure ()
 
 handleMessage :: LogFunc -> TVar BotState -> Discord.Message -> Discord.DiscordHandler ()
-handleMessage logFunction botState Discord.Message {Discord.messageText = messageText} = do
-  newMessageCount <- liftIO $
+handleMessage logFunction botState message = do
+  let messageText = Discord.messageText message
+  case decodeCommand (Discord.messageChannel message) (Discord.messageAuthor message) messageText of
+    Just command ->
+      handleCommand command logFunction botState
+    Nothing ->
+      pure ()
+  newMessageCount <- liftIO $ do
     atomically $ do
       state@BotState {messageCount = count} <- readTVar botState
       let newCount = count + 1
@@ -52,6 +61,51 @@ handleMessage logFunction botState Discord.Message {Discord.messageText = messag
       pure newCount
   runRIO logFunction $ do
     discordLog $ "Message received (" <> textDisplay newMessageCount <> "): " <> messageText
+
+data Command
+  = Login Discord.ChannelId Discord.User UUID
+  | GenerateToken Discord.ChannelId Discord.User
+  | AuthenticatedUsers Discord.ChannelId Discord.User
+
+decodeCommand :: Discord.ChannelId -> Discord.User -> Text -> Maybe Command
+decodeCommand channelId user text
+  | text == "!generate-token" = Just $ GenerateToken channelId user
+  | text == "!authenticated" = Just $ AuthenticatedUsers channelId user
+  | "!login " `Text.isPrefixOf` text =
+    case Text.split (== ' ') text of
+      _ : token : _ ->
+        case UUID.fromText token of
+          Just uuid -> Just $ Login channelId user uuid
+          Nothing -> Nothing
+      _ -> Nothing
+  | otherwise = Nothing
+
+handleCommand :: Command -> LogFunc -> TVar BotState -> Discord.DiscordHandler ()
+handleCommand (GenerateToken _channelId user) logFunction botState = do
+  newToken <- liftIO UUID.nextRandom
+  state@BotState {activeTokens = tokens} <- readTVarIO botState
+  let newTokens = Map.insert user newToken tokens
+  atomically $ writeTVar botState (state {activeTokens = newTokens})
+  runRIO logFunction $ do
+    discordLog $ "Added token '" <> tshow newToken <> "' for user with ID '" <> Discord.userName user <> "'"
+  pure ()
+handleCommand (Login channelId user suppliedToken) _ botState = do
+  state@BotState {activeTokens = tokens, authenticated = authenticatedUsers} <- readTVarIO botState
+  let userToken = Map.lookup user tokens
+  case userToken of
+    Just token
+      | token == suppliedToken -> do
+        let newState = state {authenticated = Set.insert user authenticatedUsers}
+        void $ atomically $ swapTVar botState newState
+        void $ Discord.restCall $ DiscordRequests.CreateMessage channelId "You have been authenticated."
+      | otherwise ->
+        pure ()
+    Nothing -> pure ()
+handleCommand (AuthenticatedUsers channelId user) _ botState = do
+  BotState{authenticated = authenticatedUsers} <- readTVarIO botState
+  if user `Set.notMember` authenticatedUsers then pure () else do
+    let usersString = Text.intercalate "\n" (Set.elems $ Set.map Discord.userName authenticatedUsers)
+    void $ Discord.restCall $ DiscordRequests.CreateMessage channelId usersString
 
 onStart :: LogFunc -> Discord.DiscordHandler ()
 onStart logFunc = do
