e79abf682cd05a7f2f09eae2f1532e8b8c600b2c
Rickard Andersson <gonz@severnatazvezda.com>
2021-04-25 09:49:22 +0300

    add pull to build file

--- build-docker.sh
+++ build-docker.sh
@@ -1,6 +1,9 @@
 export APP_NAME="pomoshtnik"
 export GIT_BRANCH="main"
-export DOCKER_TAG="pomoshtnik:latest"
+export DOCKER_TAG="gonz/pomoshtnik:latest"
+
+docker pull fpco/stack-build:lts-16.31
 
 docker-build-cacher build
 docker-build-cacher cache
+
